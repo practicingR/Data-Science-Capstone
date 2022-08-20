e79778329c75287b00e8c71f8dd389ae51e67fdd
practicingR <97630782+practicingR@users.noreply.github.com>
2022-08-20 09:54:17 -0400

    Update ui.R

--- ui.R
+++ ui.R
@@ -2,28 +2,22 @@ library(shiny)
 library(markdown)
 library(RColorBrewer)
 
-# Define UI for application that draws a histogram
+# Define UI for application that predicts next word based on user input
 shinyUI(fluidPage(
-
-    # Application title
-    titlePanel("Word Prediction"),
-
-    # Sidebar with ability for user to enter string of words
-    sidebarLayout(
-        sidebarPanel(
-          helpText("Enter one to four words to predict next word"),
-            textInput("inputString", "Enter words below", value= "")
-        ),
-
-        # Show the predicted word
-        mainPanel(
-            h2("Predicted Next Word"),
-            verbatimTextOutput("prediction"),
-            strong("Sentence Input:"),
-            textOutput('text1'),
-            br(),
-            strong("Note:"),
-            textOutput('text2')
-        )
+  
+  # Application title
+  titlePanel("Word Prediction"),
+  
+  # sidebar
+  sidebarLayout(
+    sidebarPanel(
+      textInput("inputString", "Enter some words here to get a predicted next word", value = "")
+    ),
+    mainPanel(
+      h2("Predicted Next Word"),
+      verbatimTextOutput("prediction"),
+      br()
     )
-))
+  )
+)
+)
